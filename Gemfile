705bfe29d11c1624ec30b4932e1df82a939ed471
Dale Palmer & Lee Jorgensen <student@epicodus.com>
2015-01-21 16:57:32 -0800

    yep

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source('https://rubygems.org')
+
+gem('rspec')
+gem('bundler')
+gem('sinatra')
+gem('sinatra-contrib')
+gem('pg')
+gem('pry')
--- app.rb
+++ app.rb
@@ -2,21 +2,24 @@ require('sinatra')
 require('sinatra/reloader')
 also_reload("lib/**/*.rb")
 require('./lib/lines')
+require('./lib/stations')
 require('./lib/stops')
 require('pg')
 
 DB = PG.connect({:dbname => "public_transit"})
 
 get("/") do
-  @lines = Lines.all()
-  @stops = Station.all()
+  @lines = Line.all()
+  @stations = Station.all()
   erb(:index)
 end
 
-post("lines") do
+post("/lines") do
   name = params.fetch("name")
   direction = params.fetch("direction")
   line_id = params.fetch("line_id").to_i()
   line = Line.new({:name => name, :direction => direction, :id => line_id})
   line.save()
-  
+  @line = Line.
+  erb(:line)
+end
